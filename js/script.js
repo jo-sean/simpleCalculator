0ab841c4cb6cb4938ad986f7825920cb792c77ce
jo-sean <joseanmanuel@gmail.com>
2023-05-09 19:22:23 -0700

    Added work to display number string

--- js/script.js
+++ js/script.js
@@ -12,17 +12,17 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-      return "Error: Cannot divide by zero";
+        return "Error: Cannot divide by zero";
     }
     return a / b;
-  }
+}
 
-function opreator(operator, a, b) {
-    switch(operator) {
+function operator(operator, a, b) {
+    switch (operator) {
         case "+":
             return add(a, b);
         case "-":
-            return subtract(a, b);    
+            return subtract(a, b);
         case "*":
             return multiply(a, b);
         case "/":
@@ -31,4 +31,19 @@ function opreator(operator, a, b) {
             return "Error: Wrong operator";
     }
 }
+
+let displayString = '0';
+
+// Displays value of displayString, 
+// cuts off string to max of 15 characters by creating a substring
+function displayContent() {
+    const display = document.getElementById('display');
+    display.innerText = displayString;
+
+    if (displayValue.length > 16) {
+        display.innerText = displayString.substring(0, 9);
+    }
+}
+
+displayContent();
 
