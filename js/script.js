90b5ea398f48cda34da65bb2cc779be272bd0a95
jo-sean <joseanmanuel@gmail.com>
2023-05-11 12:10:38 -0700

    Fixed bug with multiple numbers or changing operation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+// add, subtract, multiple, and divide functionalities for calc
 function add(a, b) {
     return a + b;
 }
+
 
 function subtract(a, b) {
     return a - b;
 }
 
+
 function multiply(a, b) {
     return a * b;
 }
+
 
 function divide(a, b) {
     if (b === 0) {
@@ -17,6 +21,8 @@ function divide(a, b) {
     return a / b;
 }
 
+
+// Controls which operation occurs between any two real numbers
 function operator(operator, a, b) {
     switch (operator) {
         case "+":
@@ -33,35 +39,47 @@ function operator(operator, a, b) {
 }
 
 
-let operationArray = {
-    firstNum: null,
-    operatorSymbol: null,
-};
+// Global array holding first number input and operation to occur
+let display = document.getElementById("display"),
+    operationArray = {
+        firstNum: null,
+        operatorSymbol: null,
+        secondNum: false,
+        operatorSequenceFlag: false
+    };
 
 
+// Adds numbers to display.value
 function addNumber(number) {
-    let display = document.getElementById("display");
     let displayText = display.value;
     if (displayText === "0" ||
-        displayText === "." ||
         displayText.match(/^[A-Za-z]*$/) ||
-        operationArray.operatorSymbol) {
+        operationArray.operatorSequenceFlag ||
+        operationArray.secondNum) {
         display.value = number;
+        operationArray.operatorSequenceFlag = false;
     } else {
         display.value = displayText + number;
     };
 };
 
-
+// Adds operator
 function addOperator(symbol) {
-    if (operationArray.firstNum && operationArray.operatorSymbol) {
+    if (operationArray.firstNum &&
+        operationArray.operatorSymbol &&
+        operationArray.secondNum) {
         equals();
-        if (operationArray.firstNum === "Yucky") { clearAll(true); }
-        else { operationArray.operatorSymbol = symbol; };
-    } else {
+        operationArray.secondNum = false;
+    } else if (!operationArray.secondNum) {
         operationArray.firstNum = Number(display.value);
         operationArray.operatorSymbol = symbol;
+        operationArray.operatorSequenceFlag = true;
+        return;
     };
+
+    // operationArray.firstNum = Number(display.value);
+    // operationArray.operatorSymbol = symbol;
+    // operationArray.operatorSequenceFlag = true;
 }
 
 
@@ -69,7 +87,7 @@ function clearAll(boolVal) {
     operationArray = {
         firstNum: null,
         operatorSymbol: null,
-        secondNum: null
+        operatorSequenceFlag: false
     };
     if (!boolVal) { display.value = '0'; };
 };
@@ -80,8 +98,9 @@ function equals() {
         let result = operator(operationArray.operatorSymbol,
             operationArray.firstNum,
             Number(display.value));
+
+        clearAll(true);
         display.value = result;
-        operationArray.firstNum = result;
     };
     return;
 };
